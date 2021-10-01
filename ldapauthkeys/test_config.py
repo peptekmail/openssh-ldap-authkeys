c05b8dcbf5d7d37d66714a53d082a80d6466761a
peptekmail <peptekmail@gmail.com>
2021-10-01 23:19:26 +0200

    Update test_config.py
    test for cert conversion

--- ldapauthkeys/test_config.py
+++ ldapauthkeys/test_config.py
@@ -1,6 +1,16 @@
 import unittest
 from ldapauthkeys.config import *
+from ldapauthkeys.util import *
+import glob
+import os
 
 class TestMyCode(unittest.TestCase):
+  def test_cert_to_sshkey(self):
+    list_of_files = glob.glob('/usr/share/ca-certificates/mozilla/*')
+    latest_file = max(list_of_files, key=os.path.getctime)
+    f = open(latest_file,'rb')
+    self.assertIsNot(cert_to_sshkey(f.read(),'')
+    f.close()
+ 
   def test_config(self):
     self.assertNotEqual(load_config(), None)
